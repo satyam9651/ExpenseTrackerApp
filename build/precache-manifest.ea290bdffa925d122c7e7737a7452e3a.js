ee78d284c68a9a4b99c16fd12bd0b10bc6fc0bc9
satyam9651 <78092596+satyam9651@users.noreply.github.com>
2023-11-07 18:15:06 +0530

    Added expense tracking application files

---
+++ build/precache-manifest.ea290bdffa925d122c7e7737a7452e3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067517e4afe0d9ebbbced336b042a316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b57aec1f7f93a63ac6a",
+    "url": "/static/css/main.6a4be60b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b57aec1f7f93a63ac6a",
+    "url": "/static/js/main.e8e584cf.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
